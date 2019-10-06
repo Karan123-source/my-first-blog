c419d792be5c1be65d4768b850e24430bbe2891b
karan <karant@iitk.ac.in>
2019-10-06 17:10:13 +0530

    css and templates design

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,9 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+def acads(request):
+    return render(request, 'blog/acads.html')
