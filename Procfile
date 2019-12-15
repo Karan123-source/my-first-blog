5fed641280a8b369c405917b54e702e7e474909a
karan <karant@iitk.ac.in>
2019-12-15 20:17:44 +0530

    Configured for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn karan_blog.wsgi --log-file -
--- karan_blog/settings.py
+++ karan_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'b2%$up4jft$x_x1#6$bv!b(0g__2ydi$b979l1)i02zq#zif&n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', '0.0.0.0']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #media images for blog posts
 
